57dbc19f0a80ff8741cf71e6a2c44b6c7701ffa5
SLFatemi <sinalotfifatemi@gmail.com>
2025-06-25 19:17:48 +0430

    Lecture #16

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,20 @@
-/* eslint-disable no-unused-vars */
 function Footer() {
     const curTime = new Date().toLocaleTimeString()
 
     const hour = new Date().getHours()
-    const openHour = 12;
+    const openHour = 11;
     const closeHour = 22
 
     let isOpen
-    isOpen = !(hour >= openHour && hour <= closeHour);
+    isOpen = (hour >= openHour && hour <= closeHour);
 
 
-    return <footer className={'footer'}>{curTime} We are currently open!</footer>
+    // return <footer className={'footer'}>{curTime} We are currently {isOpen ? 'open' : 'closed'}!</footer>
+    return <div className={'order'}>
+        <footer
+            className={'footer'}>{isOpen && `We are currently open! until ${closeHour}:00, Come visit us or order online.`}</footer>
+        <button className={'btn'}>Order</button>
+    </div>
 }
 
 export default Footer
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,15 +2,19 @@ import Pizza from "./Pizza.jsx";
 import pizzaData from "../assets/data.js";
 
 function Menu() {
+    const isTherePizza = !!pizzaData.length
+
     return <main className={'menu'}>
         <h2>Our Menu</h2>
-        <ul className={'pizzas'}>
+
+        {isTherePizza && <ul className={'pizzas'}>
             {pizzaData.map(pizza => {
                 return (
                     <Pizza pizzaObject={pizza} key={pizza.name}/>
                 )
             })}
-        </ul>
+        </ul>}
+
     </main>
 }
 
